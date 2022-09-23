04d44fe4b5adfabe7d85cf130d877aa6de66b370
Pouriya <pouriya.babaali1998@gmail.com>
2022-09-24 01:30:36 +0330

    first commit: create project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
+import 'styles/font.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
