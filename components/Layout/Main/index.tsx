9b8483ad00d0b24625af9029beb6b8045328c729
p.babaali <p.babaali@baman.club>
2022-10-09 17:25:10 +0330

    change video src to arvan

--- components/Layout/Main/index.tsx
+++ components/Layout/Main/index.tsx
@@ -6,10 +6,10 @@ type MainProps = {
 
 }
 export const Main = (props: MainProps) => {
-    const video = useRef<HTMLVideoElement>(null);
+    const video = useRef<HTMLIFrameElement>(null);
     const [videoSize, setVideoSize] = useState<number | undefined>(0);
     const handleResizeVideo = () => {
-        setVideoSize(video.current?.getClientRects()[0].height)
+        setVideoSize(video?.current?.getClientRects()[0].height)
     }
     useEffect(() => {
         handleResizeVideo();
@@ -18,20 +18,16 @@ export const Main = (props: MainProps) => {
     }, [])
     console.log(videoSize);
 
-
     return (
-        <div className="bg-primary-1000 h-full p-0 flex flex-col lg:p-5">
+        <div className="bg-primary-1000 h-full p-0 flex flex-col md:p-3 lg:p-5 ">
             <div className="hidden lg:block mb-2">
                 <Header />
             </div>
-            <div className="fixed lg:relative inset-0">
-                <div className="lg:mt-1 lg:rounded-xl overflow-hidden lg:h-full">
-                    <video controls className="w-full 2xl:w-full 3xl:w-[85%] aspect-video mx-auto" ref={video}>
-                        <source src="http://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4" />
-                        Your browser does not support HTML video.
-                    </video>
+            <div className="fixed lg:relative inset-0 overflow-hidden h-full flex flex-col lg:flex-row justify-center items-center">
+                <div className="flex justify-center items-center w-full">
+                    <iframe className="aspect-video lg:rounded-xl w-full h-full" ref={video} src="https://player.arvancloud.com/index.html?config=https://inpoint.arvanlive.com/inp/origin_config.json&skin=shaka" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true} />
                 </div>
-                <div className="bg-primary-1100 h-6/12 lg:hidden" style={{
+                <div className="bg-primary-1100 lg:hidden" style={{
                     height: `calc(100vh - ${videoSize}px)`
                 }}>
                     <Navigation />
