0e7fd3d04146ea3a09c3f28f659a2cb2277294c5
pouriya.babaali1998@gmail.com <pouriya.babaali1998@gmail.com>
2022-10-11 00:47:53 +0330

    fix : responsive

--- components/Layout/Main/index.tsx
+++ components/Layout/Main/index.tsx
@@ -17,14 +17,13 @@ export const Main = (props: MainProps) => {
         return () => window.removeEventListener("resize", handleResizeVideo);
     }, [])
 
-
     return (
         <div className="bg-primary-1000 h-full p-0 flex flex-col md:p-3 lg:p-5 ">
             <div className="hidden lg:block mb-2 xl:flex flex-shrink-0 justify-center items-center w-full">
                 <Header />
             </div>
             <div className="fixed lg:relative inset-0 overflow-hidden h-full flex flex-col lg:flex-row justify-center items-center rounded-none lg:rounded-lg">
-                <div className="flex bg-black justify-center h-full w-full max-w-full min-h-0">
+                <div className="flex relative h-auto bg-black justify-center  w-full max-w-full min-h-0">
                     <iframe className="h-full w-full aspect-video" ref={video} src="https://player.arvancloud.com/index.html?config=https://inpoint.arvanlive.com/inp/origin_config.json&skin=shaka" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true} />
                 </div>
                 <div className="bg-primary-1100 lg:hidden w-full flex-auto">
