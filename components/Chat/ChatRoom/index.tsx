5f04f0007eca69f64a77ef079f05d603e9916a9e
pouriya.babaali1998@gmail.com <pouriya.babaali1998@gmail.com>
2022-10-11 22:12:51 +0330

    check absoulte

--- components/Chat/ChatRoom/index.tsx
+++ components/Chat/ChatRoom/index.tsx
@@ -9,7 +9,7 @@ type ChatRoomProps = {
 export const ChatRoom = ({ type = 'public', videoSize }: ChatRoomProps) => {
     return (
         <ChatLayout videoSize={videoSize}>
-            <ul className="p-3 overflow-auto h-[calc(100%-57px)] scrollbar-thin scrollbar-track-slate-700 scrollbar-thumb-slate-600">
+            <ul className="p-3 overflow-auto h-[calc(100%-50px)] scrollbar-thin scrollbar-track-slate-700 scrollbar-thumb-slate-600">
                 <li className='px-5 py-2'>
                     <Message type={type} />
                 </li>
@@ -46,7 +46,7 @@ export const ChatRoom = ({ type = 'public', videoSize }: ChatRoomProps) => {
                     <Message type={type} />
                 </li>
             </ul>
-            <div className='fixed lg:absolute bottom-0 right-0 left-0 w-full'>
+            <div className='absolute lg:absolute bottom-0 right-0 left-0 w-full'>
                 <MessageBox />
             </div>
         </ChatLayout>
